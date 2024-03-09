d7ecb9334b4410f53937ae67b78065eaf9d629ae
vishal arora <vishalarora1087@gmill.com>
2024-03-09 12:57:01 +0530

    update

--- hospital_django/urls.py
+++ hospital_django/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 from hospital_django import views
 
 urlpatterns = [
-    path("",views.home,name= "home"),
+
     path("admin/", admin.site.urls),
+    
+    path("",views.home,name= "home"),
     path("aboutUS/",views.aboutUS,name= "about"),
     path("service/",views.services,name="service"),
     path("department",views.department,name="department"),
